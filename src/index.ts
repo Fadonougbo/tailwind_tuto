1144de857767fa5e2687435c4fb35df16440c5cf
Fadonougbo <fadougbogautier@gmail.com>
2024-06-17 21:48:09 +0100

    fin tuto regex

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,51 @@
 
-const inp=document.querySelector('input')
+const x="Madame Jane Doe née le 3 avril 2010  "
+
+const x2="Madame Jane Madame Jane Doe née le 3 avril 2010  "
+
+//const regex=/[1-31] (.+) \d{4} /igm
+
+const liste=[['a'],['b'],['c']];
+
+
+
+/* const inp=document.querySelector('input')
 
 
 const onChange=(e:Event)=> {
    
     const currentElement=e.currentTarget as HTMLInputElement
-
+    
     if(currentElement.files) {
         const files=currentElement.files[0]
 
-
+        console.log(files.type);
+        
         const reader=new FileReader()
 
         reader.readAsDataURL(files)
 
-        reader.addEventListener('load',(e)=> {
-            const img=document.querySelector('img')
+        if(files.type==='application/pdf') {
 
-            img?.setAttribute('src',`${reader.result}`)
-        })
+            reader.addEventListener('load',(e)=> {
+                const a=document.querySelector('a')
+                
+                a?.setAttribute('href',`${reader.result}`)
+                console.log(a);
+            })
+
+        }else {
+            reader.addEventListener('load',(e)=> {
+                const img=document.querySelector('img')
+                console.log(e.type);
+                console.log(reader);
+                img?.setAttribute('src',`${reader.result}`)
+            })
+        }
+
+        
     }
 }
 
-inp?.addEventListener('change',onChange)
+inp?.addEventListener('change',onChange) */
+
